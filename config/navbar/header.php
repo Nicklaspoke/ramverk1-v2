dca2704a3bd4c7d7e0c8189c2989a73db25830e3
Nicklaspoke <Nicklas.poke@gmail.com>
2019-11-19 16:21:10 +0100

    Changed name of JSON API in navbar

--- config/navbar/header.php
+++ config/navbar/header.php
@@ -30,6 +30,31 @@ return [
                         "url" => "redovisning/kmom02",
                         "title" => "Redovisning för kmom02.",
                     ],
+                    [
+                        "text" => "Kmom03",
+                        "url" => "redovisning/kmom03",
+                        "title" => "Redovisning för kmom03.",
+                    ],
+                    [
+                        "text" => "Kmom04",
+                        "url" => "redovisning/kmom04",
+                        "title" => "Redovisning för kmom04.",
+                    ],
+                    [
+                        "text" => "Kmom04",
+                        "url" => "redovisning/kmom04",
+                        "title" => "Redovisning för kmom04.",
+                    ],
+                    [
+                        "text" => "Kmom05",
+                        "url" => "redovisning/kmom05",
+                        "title" => "Redovisning för kmom05.",
+                    ],
+                    [
+                        "text" => "Kmom07-10",
+                        "url" => "redovisning/kmom10",
+                        "title" => "Redovisning för kmom07-10.",
+                    ],
                 ],
             ],
         ],
@@ -59,7 +84,7 @@ return [
             "title" => "Validate Ip Addresses",
         ],
         [
-            "text" => "Ip Validation",
+            "text" => "JSON API",
             "url" => "validate-ip-json",
             "title" => "Validate Ip Addresses With JSON",
         ],
