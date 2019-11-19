7136f3a425c8817a6b348748861f39d292f7e556
Nicklaspoke <Nicklas.poke@gmail.com>
2019-11-19 16:22:09 +0100

    User ip is now prefilled in the input

--- view/ip-validation/index.php
+++ view/ip-validation/index.php
@@ -2,11 +2,32 @@
 <br>
 <h1>Ip Validation</h1>
 
-<form method="POST">
-    <label for="ipInput">Ipv4 or Ipv6 address to validate:</label>
-    <input type="text" name="ipInput" value="8.8.8.8">
-    <input type="submit" value="Validate">
-</form>
+<table>
+    <tr>
+        <th>kmom01</th>
+        <th>kmom02</th>
+    </tr>
+
+    <tr>
+        <td>
+            <form method="POST">
+                <label for="ipInput">Ipv4 or Ipv6 address to validate:</label>
+                <input type="text" name="ipInput" value=<?= isset($userIp) ? $userIp : "8.8.8.8" ?>>
+                <!-- <input type="submit" value="Validate"> -->
+                <button name="kmom" type="submit" value="kmom01">Validate</button>
+            </form>
+        </td>
+
+        <td>
+            <form method="POST">
+                <label for="ipInput">Ipv4 or Ipv6 address to validate:</label>
+                <input type="text" name="ipInput" value=<?= isset($userIp) ? $userIp : "8.8.8.8" ?>>
+                <!-- <input type="submit" value="Validate"> -->
+                <button name="kmom" type="submit" value="kmom02">Validate</button>
+            </form>
+        </td>
+    </tr>
+</table>
 
 <?php
 if (!empty($message)) {
